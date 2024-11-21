5947a5c7973d8ba29a74532d2e8f62b990aea579
JoseRGomez05 <rafa-0512@hotmail.com>
2024-11-21 11:48:17 -0500

    17

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function mostarmensaje(){
     alert("Hola!!!, Bienvenidos...");
 }
 
+function validarFormulario(){
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    
+    if (nombre === "" || contraseña===""|| email==="") {
+        alert("Debe completar todos los campos que tengan un *")
+        return false;
+    }
+    return true;
+}
